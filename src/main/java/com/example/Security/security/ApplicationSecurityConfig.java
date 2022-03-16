74ed9f3bca882dc8e56177a0ad4c575fd99ff303
raul <rauljaciel_91@hotmail.com>
2022-03-15 21:03:15 -0600

    proyecto creado

---
+++ src/main/java/com/example/Security/security/ApplicationSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.Security.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()//any request must be authenticated
+                .authenticated()//must declare username and password
+                .and()
+                .httpBasic();
+    }
+}
