2de53026a475bc533b4124a139362ccd34dc3e93
raul <rauljaciel_91@hotmail.com>
2022-03-16 16:29:55 -0600

    Urls allowed with antMatchers

--- src/main/java/com/example/Security/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/Security/security/ApplicationSecurityConfig.java
@@ -12,6 +12,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*")
+                .permitAll()
                 .anyRequest()//any request must be authenticated
                 .authenticated()//must declare username and password
                 .and()
